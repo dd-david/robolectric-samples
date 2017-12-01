a5c3cbdcf800f31b854c44c63a632b0c7c273c79
dd-david <dev.dream.developer@gmail.com>
2017-12-02 00:18:48 +0900

    Robolectric: unit test for Receiver #1

--- android-api-21/src/test/java/com/example/receiver/BootCompleteReceiverTest.java
+++ android-api-21/src/test/java/com/example/receiver/BootCompleteReceiverTest.java
@@ -1,6 +1,7 @@
 package com.example.receiver;
 
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 
 import com.example.BuildConfig;
 
@@ -10,6 +11,7 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowApplication;
 
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 @RunWith(RobolectricGradleTestRunner.class)
@@ -18,11 +20,30 @@ public class BootCompleteReceiverTest {
 
     @Test
     public void registerServiceOnDeviceBootComplete() {
-        Intent intent = new Intent(Intent.ACTION_BOOT_COMPLETED);
 
+        /*
+        * Receiver 를 통해서 실제로 수신할 Intent 를 정의합니다
+        * (Intent.ACTION_BOOT_COMPLETED)
+        *
+        * */
+        Intent intentBootCompleted = new Intent(Intent.ACTION_BOOT_COMPLETED);
+        Intent intentScanResultsAvailableAction = new Intent(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+
+        /*
+        * org.robolectric.shadows.ShadowApplication
+        * Todo: ShadowApplication 은 정확히 무슨 용도로 사용되는지 확인합시다
+        * 앞선 예제(Service) 에서는 RuntimeEnvironment.application 으로 Application 객체를 가져옴
+        * */
         ShadowApplication application = ShadowApplication.getInstance();
 
-        assertTrue("Reboot Listener not registered ",
-                application.hasReceiverForIntent(intent));
+        /*
+        * 단순히 Application 이 해당 Intent 에 대한 Receiver 가 있는지만 바라보는건가? 너무 심플
+        * */
+        assertTrue("Reboot Listener not registered ", application.hasReceiverForIntent(intentBootCompleted));
+        assertFalse("Wi-Fi Scan Listener not registered ", application.hasReceiverForIntent(intentScanResultsAvailableAction));
+
+        BootCompleteReceiver receiver = new BootCompleteReceiver();
+        receiver.onReceive(application.getApplicationContext(), intentBootCompleted);
+        assertTrue(true);
     }
 }
